5536db284290eee97793ef11cab49501384610bf
Matteo DeSimone <thematteod@gmail.com>
2022-09-14 19:20:24 -0400

    Initial commit

---
+++ PracticeLinqProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinqProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>() { "Skyrim", "Assassin's Creed", "Dark Souls", "Cyberpunk 2077", 
+                "Far Cry", "God of War", "Red Dead Redemption", "Grand Theft Auto", "Mafia", "The Division", "Ghost Recon" };
+            var sorted = videoGames.OrderBy(x => x.Length);
+            
+
+            foreach (var game in sorted)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
